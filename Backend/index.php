5399317a118f3169a645d9b621263e1542d39926
RedOwl <66703097+RedOwlllll@users.noreply.github.com>
2024-09-28 21:23:11 +1200

    Setting up backend to connect to Azure Server
    This is the backend server to connect to Azure, user need to install php and install proper extension and driver from Azure to be able to run the server.

---
+++ Backend/index.php
@@ -0,0 +1,12 @@
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:adaptive-network-management.database.windows.net,1433; Database = Adaptive_Netowork_Management", "CloudSA44398b14", "Adaptive2024");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // If the connection is successful
+    echo "Successfully connected to SQL Server!";
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
